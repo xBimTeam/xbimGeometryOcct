af7ff64d424ee8db5ebef41c9ebdcf95eb34896b
Andy Ward <andy.ward@xbim.net>
2025-01-09 18:56:17 +0000

    WIP: Build Update - Modified Projects

--- Builds/src/TKGeomAlgo/cmake_install.cmake
+++ Builds/src/TKGeomAlgo/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/OCCT-Build/OCCT-7_7_0/src/TKGeomAlgo
+# Install script for directory: C:/OCCT-Build/OCCT-7_8_0/src/TKGeomAlgo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
