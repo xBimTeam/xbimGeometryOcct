088a599f37c9e38130c67aca7b96edffea9c4c14
Andy Ward <andy.ward@xbim.net>
2025-01-13 12:16:32 +0000

    Fixed cleaning syntax to clear ignored content

--- clean.cmd
+++ clean.cmd
@@ -2,5 +2,5 @@ echo off
 
 rem Clean all untracked content in Install and Builds. Useful when switching between OCCT releases
 
-git clean -df ./Install/
-git clean -df ./Builds/
+git clean -dfX ./Install/
+git clean -dfX ./Builds/
